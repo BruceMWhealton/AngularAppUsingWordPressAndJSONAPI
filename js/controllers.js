537ba2a621a0f4f038bfbecc65faad31e0afc789
Bruce Whealton <futurewavewebdevelopment@gmail.com>
2014-09-09 20:05:36 -0400

    Modified references to artistControllers to newsControllers.  artistControllers was referring to a different project.

--- js/controllers.js
+++ js/controllers.js
@@ -1,60 +1,36 @@
-var artistControllers = angular.module('blogsControllers', []);
+var newsControllers = angular.module('newsControllers', []);
 
-artistControllers.controller('CoverController', ['$scope', function($scope) {
+newsControllers.controller('CoverController', ['$scope', function($scope) {
   
 }]);
 
 
-
-
-// artistControllers.controller("TechNewsController",['$scope','$http',function($scope,$http){  
-  
-//   $http({
-//     url : 'http://www.corsproxy.com/futurewavewebdevelopment.com/wp/tech-news/?json=1&count=20',
-//     method : "GET"    
-//   }).then(function(response){
-//     $scope.blogsTechNews=response.data.posts;
-//   });
-  
-// }]);
-
-artistControllers.controller('TechNewsController', ['$scope', '$http', function($scope, $http) {
-  $http.get('http://www.corsproxy.com/futurewavewebdevelopment.com/wp/tech-news/?json=1&count=20').success(function(data){
+newsControllers.controller('TechNewsController', ['$scope', '$http', function($scope, $http) {
+  $http.get('http://www.corsproxy.com/futurewavewebdevelopment.com/wp/tech-news/?json=1&count=100').success(function(data){
 
     $scope.blogsTechNews=data;
-    //$scope.artistOrder  = 'name';
+
 
   });  
 }]);
 
 
+// newsControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+//   $http.get('js/data.json').success(function(data){
+//   	$scope.artists 		= data;
+//   	$scope.whichItem 	= $routeParams.itemId; 
 
+//   	if ($routeParams.itemId > 0) {
+//       $scope.prevItem = Number($routeParams.itemId)-1;
+//     } else {
+//       $scope.prevItem = $scope.artists.length-1;
+//     }
 
-artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
-  $http.get('js/data.json').success(function(data){
+//     if ($routeParams.itemId < $scope.artists.length-1) {
+//       $scope.nextItem = Number($routeParams.itemId)+1;
+//     } else {
+//       $scope.nextItem = 0;
+//     }
 
-  	$scope.artists 		= data;
-  	$scope.artistOrder 	= 'name';
-
-  });  
-}]);
-
-artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-  $http.get('js/data.json').success(function(data){
-  	$scope.artists 		= data;
-  	$scope.whichItem 	= $routeParams.itemId; 
-
-  	if ($routeParams.itemId > 0) {
-      $scope.prevItem = Number($routeParams.itemId)-1;
-    } else {
-      $scope.prevItem = $scope.artists.length-1;
-    }
-
-    if ($routeParams.itemId < $scope.artists.length-1) {
-      $scope.nextItem = Number($routeParams.itemId)+1;
-    } else {
-      $scope.nextItem = 0;
-    }
-
-  });  
-}]);
+//   });  
+// }]);  
